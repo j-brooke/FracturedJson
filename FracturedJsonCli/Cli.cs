d757673f90c9d1ad90c27905d7ecca9137baab3b
Jesse Brooke <6962885+j-brooke@users.noreply.github.com>
2022-09-22 19:20:48 -0400

    A couple quick hacks to see results so far.

--- FracturedJsonCli/Cli.cs
+++ FracturedJsonCli/Cli.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
-using System.Text.Json;
-using Mono.Options;
-using FracturedJson;
+using FracturedJson.V3;
 
 namespace FracturedJsonCli
 {
@@ -17,81 +14,16 @@ namespace FracturedJsonCli
         {
             try
             {
-                var formatter = new Formatter();
-
-                bool showHelp = false;
-                string? fileName = null;
-                bool noPadding = false;
-
-                var cliOpts = new OptionSet()
+                var inputText = File.ReadAllText(args[0]);
+                var options = new FracturedJsonOptions()
                 {
-                    {
-                        "a|array=",
-                        "table array minimum similarity",
-                        (double d) => formatter.TableArrayMinimumSimilarity = d
-                    },
-                    {"c|complexity=", "maximum inline complexity", (int n) => formatter.MaxInlineComplexity = n},
-                    {"e|expand=", "always-expand depth", (int n) => formatter.AlwaysExpandDepth = n},
-                    {"f|file=", "input from file instead of stdin", s => fileName = s},
-                    {"h|help", "show this help info and exit", v => showHelp = (v != null)},
-                    {"j|no-justify", "don't justify parallel numbers", v => formatter.DontJustifyNumbers = true},
-                    {"l|length=", "maximum inline length", (int n) => formatter.MaxInlineLength = n},
-                    {
-                        "m|multiline=",
-                        "maximum multi-line array complexity",
-                        (int n) => formatter.MaxCompactArrayComplexity = n
-                    },
-                    {
-                        "o|object=",
-                        "table object minimum similarity",
-                        (double d) => formatter.TableObjectMinimumSimilarity = d
-                    },
-                    {"p|no-padding", "don't include padding spaces", v => noPadding = (v != null)},
-                    {
-                        "s|space=",
-                        "use this many spaces per indent level",
-                        (int n) => formatter.IndentSpaces = n
-                    },
-                    {"t|tab", "use tabs for indentation", _ => formatter.UseTabToIndent = true},
-                    {"u|unix", "use Unix line endings (LF)", v => formatter.JsonEolStyle = EolStyle.Lf},
-                    {"w|windows", "use Windows line endings (CRLF)", v => formatter.JsonEolStyle = EolStyle.Crlf},
+                    CommentPolicy = CommentPolicy.Preserve,
+                    PreserveBlankLines = true,
+                    MaxTotalLineLength = 800,
+                    MaxInlineLength = 120,
                 };
-
-                cliOpts.Parse(args);
-
-                if (showHelp)
-                {
-                    ShowHelp(cliOpts);
-                    return 0;
-                }
-
-                if (noPadding)
-                {
-                    formatter.ColonPadding = false;
-                    formatter.CommaPadding = false;
-                    formatter.NestedBracketPadding = false;
-                }
-
-                JsonDocument jsonDocument;
-                var docOpts = new JsonDocumentOptions() { CommentHandling = JsonCommentHandling.Skip };
-
-                if (fileName != null)
-                {
-                    using var stream = File.OpenRead(fileName);
-                    jsonDocument = JsonDocument.Parse(stream, docOpts);
-                }
-                else if (Console.IsInputRedirected)
-                {
-                    var inputData = Console.In.ReadToEnd();
-                    jsonDocument = JsonDocument.Parse(inputData, docOpts);
-                }
-                else
-                {
-                    ShowHelp(cliOpts);
-                    return 1;
-                }
-
-                var formattedDoc = formatter.Serialize(jsonDocument);
+                var formatter = new Formatter() { Options = options };
+                var formattedDoc = formatter.Reformat(inputText, 0);
                 Console.WriteLine(formattedDoc);
                 return 0;
             }
@@ -100,25 +32,6 @@ namespace FracturedJsonCli
                 Console.Error.WriteLine(ex.ToString());
                 return 1;
             }
-        }
-
-        private static void ShowHelp(OptionSet cliOptions)
-        {
-            var lines = new[]
-            {
-                $"Version {Assembly.GetExecutingAssembly().GetName().Version}",
-                "Usage:",
-                "  FracturedJsonCli [OPTIONS]",
-                "",
-                "Formats JSON to stdout from stdin, or from a file if --file is used.",
-                "",
-                "Options:",
-            };
-
-            foreach (var line in lines)
-                Console.Out.WriteLine(line);
-
-            cliOptions.WriteOptionDescriptions(Console.Out);
         }
     }
 }
