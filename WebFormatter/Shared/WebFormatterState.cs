ba5795cc1030425f92e0029277f1ad1fb0d023da
Jesse Brooke <6962885+j-brooke@users.noreply.github.com>
2022-10-02 22:28:52 -0400

    Added crude layout to WebFormatter

--- WebFormatter/Shared/WebFormatterState.cs
+++ WebFormatter/Shared/WebFormatterState.cs
@@ -4,7 +4,34 @@ namespace WebFormatter.Shared;
 
 public class WebFormatterState
 {
-    public FracturedJsonOptions Options { get; set; } = new();
+    public FracturedJsonOptions Options { get; set; }
     public string InputJson { get; set; } = string.Empty;
     public string OutputJson { get; set; } = string.Empty;
+
+    public WebFormatterState()
+    {
+        Options = new()
+        {
+            MaxInlineLength = 500000,
+            MaxTotalLineLength = 100,
+            CommentPolicy = CommentPolicy.Preserve,
+            PreserveBlankLines = true,
+        };
+        _formatter = new() { Options = Options };
+    }
+
+    public void DoFormat()
+    {
+        try
+        {
+            _formatter.Options = Options;
+            OutputJson = _formatter.Reformat(InputJson, 0);
+        }
+        catch (FracturedJsonException e)
+        {
+            OutputJson = e.Message;
+        }
+    }
+
+    private readonly Formatter _formatter;
 }
