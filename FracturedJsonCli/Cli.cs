b76a9e487eb1f9b5f927ec0d982bce3b74a37a13
j-brooke <6962885+j-brooke@users.noreply.github.com>
2025-12-12 14:34:22 -0500

    Working on adding a config file option to the cli.

--- FracturedJsonCli/Cli.cs
+++ FracturedJsonCli/Cli.cs
@@ -8,143 +8,65 @@ using Mono.Options;
 namespace FracturedJsonCli
 {
     /// <summary>
-    /// Commandline app to format JSON using FracturedJson.Formatter.  Output is to standard out.
-    /// Input is from either standard in, or from a file if using the --file switch.
+    /// Commandline app to format JSON using FracturedJson.Formatter.  Output is to standard out, or a file specified
+    /// by the --outfile switch.  Input is from either standard in, or from a file if using the --file switch.
     /// </summary>
-    internal static class Cli
+    public class Cli
     {
-        internal static int Main(string[] args)
+        public int Run(string[] args)
+        {
+            DetermineSettings(args);
+
+            string inputText;
+            if (_inputFile != null)
+            {
+                inputText = File.ReadAllText(_inputFile);
+            }
+            else if (Console.IsInputRedirected)
+            {
+                inputText = Console.In.ReadToEnd();
+            }
+            else
+            {
+                Console.Error.WriteLine("Please provide an input file with -f or --file, or pipe some in.");
+                return 1;
+            }
+
+            var timer = Stopwatch.StartNew();
+
+            if (_outputFile == null)
+            {
+                var formatter = new Formatter() { Options = _fjOpts };
+                if (_minify)
+                    formatter.Minify(inputText, Console.Out);
+                else
+                    formatter.Reformat(inputText, 0, Console.Out);
+            }
+            else
+            {
+                var formatter = new Formatter() { Options = _fjOpts };
+                using var writer = File.CreateText(_outputFile);
+                if (_minify)
+                    formatter.Minify(inputText, writer);
+                else
+                    formatter.Reformat(inputText, 0, writer);
+            }
+
+            timer.Stop();
+
+            if (_speedTest)
+                Console.WriteLine($"Processing time: {timer.Elapsed.TotalSeconds} sec");
+
+            return 0;
+        }
+
+        public static int Main(string[] args)
         {
             try
             {
-                var options = FracturedJsonOptions.Recommended();
-
-                var showHelp = false;
-                string? fileName = null;
-                var noPadding = false;
-                var allowComments = false;
-                var speedTest = false;
-                string? outFileName = null;
-                var minify = false;
-
-                var cliOpts = new OptionSet()
-                {
-                    { "a|allow", "allow comments and trailing commas", _ => allowComments = true },
-                    { "c|complexity=", "maximum inline complexity", (int n) => options.MaxInlineComplexity = n },
-                    { "e|expand=", "always-expand depth", (int n) => options.AlwaysExpandDepth = n },
-                    { "f|file=", "input from file instead of stdin", s => fileName = s },
-                    { "h|help", "show this help info and exit", _ => showHelp = true },
-                    {
-                        "j|justify=", "number list justification [l,r,d,n]", s =>
-                            options.NumberListAlignment = s.ToUpper() switch
-                        {
-                            "L" or "LEFT" => NumberListAlignment.Left,
-                            "R" or "RIGHT" => NumberListAlignment.Right,
-                            "D" or "DECIMAL" => NumberListAlignment.Decimal,
-                            "N" or "NORMALIZE" => NumberListAlignment.Normalize,
-                            _ => NumberListAlignment.Left,
-                        }
-                    },
-                    {
-                        "k|comma=", "table comma placement [b,a,n]", s =>
-                            options.TableCommaPlacement = s.ToUpper() switch
-                            {
-                                "B" or "BEFORE" => TableCommaPlacement.BeforePadding,
-                                "A" or "AFTER" => TableCommaPlacement.AfterPadding,
-                                "N" or "NUMBER" => TableCommaPlacement.BeforePaddingExceptNumbers,
-                                _ => TableCommaPlacement.AfterPadding,
-                            }
-                    },
-                    {
-                        "l|length=",
-                        "maximum total line length when inlining",
-                        (int n) => options.MaxTotalLineLength = n
-                    },
-                    {
-                        "m|multiline=",
-                        "maximum multi-line array complexity",
-                        (int n) => options.MaxCompactArrayComplexity = n
-                    },
-                    { "n|name-padding=", "max property name padding", (int n) => options.MaxPropNamePadding = n},
-                    { "o|outfile=", "write output to file", s => outFileName = s },
-                    { "p|no-padding", "don't include padding spaces", _ => noPadding = true },
-                    {
-                        "s|space=",
-                        "use this many spaces per indent level",
-                        (int n) => options.IndentSpaces = n
-                    },
-                    { "r|row=", "maximum table row complexity", (int n) => options.MaxTableRowComplexity = n },
-                    { "t|tab", "use tabs for indentation", _ => options.UseTabToIndent = true },
-                    { "u|unix", "use Unix line endings (LF)", _ => options.JsonEolStyle = EolStyle.Lf },
-                    { "w|windows", "use Windows line endings (CRLF)", _ => options.JsonEolStyle = EolStyle.Crlf },
-                    { "minify", "remove unnecessary space but preserve comments", _ => minify = true },
-                    { "z|speed-test", "write timer data instead of JSON output", _ => speedTest = true },
-                };
-
-                cliOpts.Parse(args);
-
-                if (showHelp)
-                {
-                    ShowHelp(cliOpts);
-                    return 0;
-                }
-
-                if (noPadding)
-                {
-                    options.ColonPadding = false;
-                    options.CommaPadding = false;
-                    options.CommentPadding = false;
-                    options.NestedBracketPadding = false;
-                }
-
-                if (allowComments)
-                {
-                    options.CommentPolicy = CommentPolicy.Preserve;
-                    options.AllowTrailingCommas = true;
-                    options.PreserveBlankLines = true;
-                }
-
-                string inputText;
-                if (fileName != null)
-                {
-                    inputText = File.ReadAllText(fileName);
-                }
-                else if (Console.IsInputRedirected)
-                {
-                    inputText = Console.In.ReadToEnd();
-                }
-                else
-                {
-                    ShowHelp(cliOpts);
-                    return 1;
-                }
-
-                var timer = Stopwatch.StartNew();
-
-                if (outFileName == null)
-                {
-                    var formatter = new Formatter() { Options = options };
-                    if (minify)
-                        formatter.Minify(inputText, Console.Out);
-                    else
-                        formatter.Reformat(inputText, 0, Console.Out);
-                }
-                else
-                {
-                    var formatter = new Formatter() { Options = options };
-                    using var writer = File.CreateText(outFileName);
-                    if (minify)
-                        formatter.Minify(inputText, writer);
-                    else
-                        formatter.Reformat(inputText, 0, writer);
-                }
-
-                timer.Stop();
-
-                if (speedTest)
-                    Console.WriteLine($"Processing time: {timer.Elapsed.TotalSeconds} sec");
-
-                return 0;
+                var app = new Cli();
+                var rtnVal = app.Run(args);
+                return rtnVal;
             }
             catch (Exception ex)
             {
@@ -153,7 +75,100 @@ namespace FracturedJsonCli
             }
         }
 
-        private static void ShowHelp(OptionSet cliOptions)
+        private FracturedJsonOptions _fjOpts = new FracturedJsonOptions();
+        private string? _inputFile;
+        private string? _outputFile;
+        private bool _minify;
+        private bool _speedTest;
+
+        private bool DetermineSettings(string[] args)
+        {
+            var showHelp = false;
+            var noPadding = false;
+            var allowComments = false;
+
+            var cliOpts = new OptionSet()
+            {
+                { "a|allow", "allow comments and trailing commas", _ => allowComments = true },
+                { "c|complexity=", "maximum inline complexity", (int n) => _fjOpts.MaxInlineComplexity = n },
+                { "e|expand=", "always-expand depth", (int n) => _fjOpts.AlwaysExpandDepth = n },
+                { "f|file=", "input from file instead of stdin", s => _inputFile = s },
+                { "h|help", "show this help info and exit", _ => showHelp = true },
+                {
+                    "j|justify=", "number list justification [l,r,d,n]", s =>
+                        _fjOpts.NumberListAlignment = s.ToUpper() switch
+                    {
+                        "L" or "LEFT" => NumberListAlignment.Left,
+                        "R" or "RIGHT" => NumberListAlignment.Right,
+                        "D" or "DECIMAL" => NumberListAlignment.Decimal,
+                        "N" or "NORMALIZE" => NumberListAlignment.Normalize,
+                        _ => NumberListAlignment.Left,
+                    }
+                },
+                {
+                    "k|comma=", "table comma placement [b,a,n]", s =>
+                        _fjOpts.TableCommaPlacement = s.ToUpper() switch
+                        {
+                            "B" or "BEFORE" => TableCommaPlacement.BeforePadding,
+                            "A" or "AFTER" => TableCommaPlacement.AfterPadding,
+                            "N" or "NUMBER" => TableCommaPlacement.BeforePaddingExceptNumbers,
+                            _ => TableCommaPlacement.AfterPadding,
+                        }
+                },
+                {
+                    "l|length=",
+                    "maximum total line length when inlining",
+                    (int n) => _fjOpts.MaxTotalLineLength = n
+                },
+                {
+                    "m|multiline=",
+                    "maximum multi-line array complexity",
+                    (int n) => _fjOpts.MaxCompactArrayComplexity = n
+                },
+                { "n|name-padding=", "max property name padding", (int n) => _fjOpts.MaxPropNamePadding = n},
+                { "o|outfile=", "write output to file", s => _outputFile = s },
+                { "p|no-padding", "don't include padding spaces", _ => noPadding = true },
+                {
+                    "s|space=",
+                    "use this many spaces per indent level",
+                    (int n) => _fjOpts.IndentSpaces = n
+                },
+                { "r|row=", "maximum table row complexity", (int n) => _fjOpts.MaxTableRowComplexity = n },
+                { "t|tab", "use tabs for indentation", _ => _fjOpts.UseTabToIndent = true },
+                { "u|unix", "use Unix line endings (LF)", _ => _fjOpts.JsonEolStyle = EolStyle.Lf },
+                { "w|windows", "use Windows line endings (CRLF)", _ => _fjOpts.JsonEolStyle = EolStyle.Crlf },
+                { "minify", "remove unnecessary space but preserve comments", _ => _minify = true },
+                { "z|speed-test", "write timer data instead of JSON output", _ => _speedTest = true },
+            };
+
+            cliOpts.Parse(args);
+
+            if (showHelp)
+            {
+                ShowHelpPrefix();
+                cliOpts.WriteOptionDescriptions(Console.Out);
+                return false;
+            }
+
+            if (noPadding)
+            {
+                _fjOpts.ColonPadding = false;
+                _fjOpts.CommaPadding = false;
+                _fjOpts.CommentPadding = false;
+                _fjOpts.NestedBracketPadding = false;
+            }
+
+            if (allowComments)
+            {
+                _fjOpts.CommentPolicy = CommentPolicy.Preserve;
+                _fjOpts.AllowTrailingCommas = true;
+                _fjOpts.PreserveBlankLines = true;
+            }
+
+            return true;
+        }
+
+        private static void ShowHelpPrefix()
         {
             var lines = new[]
             {
@@ -168,8 +183,6 @@ namespace FracturedJsonCli
 
             foreach (var line in lines)
                 Console.Out.WriteLine(line);
-
-            cliOptions.WriteOptionDescriptions(Console.Out);
         }
     }
 }
